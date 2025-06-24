be8f3fdba2acda2fb2231c8d1e373803a73bc6cc
youssef esmail <youssef.esmailelfeky@gmail.com>
2025-06-24 21:13:53 +0300

    Initial Commit

---
+++ AroundEgypt/AroundEgyptApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgyptApp.swift
+//  AroundEgypt
+//
+//  Created by Asset's Macbook Pro on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
