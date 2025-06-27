8af3f7fa3f28e9aad389e5343613557f5029b161
youssef esmail <youssef.esmailelfeky@gmail.com>
2025-06-27 16:25:29 +0300

    fix issues of font not found in bundle , show error on base view

--- AroundEgypt/Core/BaseView.swift
+++ AroundEgypt/Core/BaseView.swift
@@ -18,6 +18,8 @@ struct BaseView<Content>: View where Content: View {
     // MARK: - PROPERTIES
     let content: Content
     @Binding private var state: ViewModelState<NetworkError>
+    @State private var showErrorAlert = false
+    @State private var errorMessage: String = ""
     // MARK: - INIT
     public init(
         state: Binding<ViewModelState<NetworkError>>,
@@ -36,5 +38,19 @@ struct BaseView<Content>: View where Content: View {
                 Spacer()
             }
         }
+        .onChange(of: state) { oldState, newState  in
+            if case let .failed(error) = newState {
+                errorMessage = error.localizedDescription 
+                showErrorAlert = true
+            } else {
+                errorMessage = ""
+                showErrorAlert = false
+            }
+        }
+        .alert("Error", isPresented: $showErrorAlert, actions: {
+            Button("OK", role: .cancel) {}
+        }, message: {
+            Text(errorMessage)
+        })
     }
 }
